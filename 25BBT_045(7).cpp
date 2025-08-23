e09371009bcf6f244a3a1b4435f5a72b2fd859cf
Bhatt stavan <stavanbhatt1318@gmail.com>
2025-08-23 11:02:33 +0530

    It's an order

---
+++ 25BBT_045(7).cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+float dollars, rupees ;
+clrscr();
+printf("Enter value of dollars");
+scanf("%f,& dollars");
+rupees=48*dollars ;
+getch();
+return 0;
+}
